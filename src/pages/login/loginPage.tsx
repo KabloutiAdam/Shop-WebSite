ad83a40f0c364beda4ef8a399ed1a48f000ac3ba
Adam Kablouti <adam.kablouti@eduvaud.ch>
2025-03-19 08:47:38 +0100

    [fix] auto resize height login form

--- src/pages/login/loginPage.tsx
+++ src/pages/login/loginPage.tsx
@@ -1,7 +1,7 @@
 
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import LoginForm from "@/components/form/loginform"
+import LoginForm from "@/components/form/loginForm"
 import RegisterForm from "@/components/form/registerForm"
 
 import '../../App.css'
@@ -12,16 +12,16 @@ function LoginPage() {
 
     return (
         <>
-            <Tabs defaultValue="account" className="relative w-[400px] bg-white rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5 min-h-[650px]">
+            <Tabs defaultValue="login" className="w-[400px] bg-white rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5 flex flex-col fixed top-50">
                 <TabsList className="self-center">
                     <TabsTrigger value="login">Connexion</TabsTrigger>
                     <TabsTrigger value="register">Créer un compte</TabsTrigger>
                 </TabsList>
-                <div className="relative w-full h-full mt-4">
-                    <TabsContent value="login" className="absolute w-full top-0 left-0">
+                <div className="w-full h-full mt-4">
+                    <TabsContent value="login" className="w-full top-0 left-0">
                         <LoginForm />
                     </TabsContent>
-                    <TabsContent value="register" className=" absolute w-full top-0 left-0">
+                    <TabsContent value="register" className="w-full top-0 left-0">
                         <RegisterForm />
                     </TabsContent>
                 </div>
