adce3e37b3bee2109852f190a2f78ad3e2fb8dd5
Adam Kablouti <111990352+KabloutiAdam@users.noreply.github.com>
2025-03-22 10:49:50 +0100

    add products

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -11,15 +11,36 @@ export default function Footer(){
                     <p className="pt-1 pb-1">Conditions générales</p>
                     <p className="pt-1 pb-1">Conditions générales de vente</p>
                     <p className="pt-1 pb-1">Protection des données</p>
+                    <p className="pt-1 pb-1">Politique de garantie</p>
+                    <p className="pt-1 pb-1">Mentions légales</p>
+                    <p className="pt-1 pb-1">Politique de confidentialité</p>
                 </div>
                 <div className="h-full w-[18%]  text-left pl-3 ">
                     <p className="font-bold uppercase mb-2">Aide et Service</p>
+                    <p className="pt-1 pb-1">Modes de paiement</p>
+                    <p className="pt-1 pb-1">Livraison et retrait</p>
+                    <p className="pt-1 pb-1">Retour produit</p>
+                    <p className="pt-1 pb-1">Assurance produit</p>
                 </div>
                 <div className="h-full w-[18%]  text-left pl-3 ">
-                    <p className="font-bold uppercase mb-2">No magasins</p>
+                    <p className="font-bold uppercase mb-2">Nos magasins</p>
+                    <p className="pt-1 pb-1">Morges</p>
+                    <p className="pt-1 pb-1">Gland</p>
+                    <p className="pt-1 pb-1">Lausanne</p>
+                    <p className="pt-1 pb-1">Genève</p>
+                    <p className="pt-1 pb-1">Bâle</p>
+                    <p className="pt-1 pb-1">Saint-Gall</p>
+                    <p className="pt-1 pb-1">Winterthour</p>
+                    <p className="pt-1 pb-1">Nyon</p>
+                    <p className="pt-1 pb-1">Montreux</p>
                 </div>
                 <div className="h-full w-[18%]  text-left pl-3 ">
                     <p className="font-bold uppercase mb-2">Contacts</p>
+                    <p className="pt-1 pb-1">Twitter</p>
+                    <p className="pt-1 pb-1">Instagram</p>
+                    <p className="pt-1 pb-1">Facebook</p>
+                    <p className="pt-1 pb-1">LinkedIn</p>
+                    <p className="pt-1 pb-1">Youtube</p>
                 </div>
 
                 
