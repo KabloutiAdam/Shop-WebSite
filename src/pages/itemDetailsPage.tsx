f9ab08442ea623961df7bc5b6d664e7f26733633
Adam Kablouti <adam.kablouti@eduvaud.ch>
2025-04-04 13:39:51 +0200

    Update itemDetailsPage.tsx

--- src/pages/itemDetailsPage.tsx
+++ src/pages/itemDetailsPage.tsx
@@ -1,32 +1,21 @@
-import { PRODUCTS } from "@/database/products";
 
-import { useParams } from "react-router-dom";
+
 
 
 
 export default function ItemDetailsPage() {
 
-    const { category, tag, id } = useParams<{ category: string, tag: string, id: string }>();
 
 
 
 
-    const product = PRODUCTS.items.find(
-        (item) =>
-            item.id === Number(id) &&
-            item.category === category &&
-            item.item === tag
-    );
-
-    if (!product) {
-        return <div>Product not found</div>
-    }
+    
 
     return (
         <div>
             <h1>Item Details Page</h1>
 
-            <p>Item ID: {product.name}</p>
+            <p>Item ID: HAAAAAAAAAAAAA</p>
 
 
 
